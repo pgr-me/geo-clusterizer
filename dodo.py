dc8cf9ee11bc56149f3de69ad1366307e8c3a583
Peter Rasmussen <pgr-me@users.noreply.github.com>
2020-09-25 22:14:53 -0400

    More structuring

--- dodo.py
+++ dodo.py
@@ -4,20 +4,20 @@ import getpass
 import os
 from pathlib import Path
 import time
+
 # third-party imports
 from doit.tools import config_changed, create_folder, run_once
 from loguru import logger
 import pandas as pd
+
 # local imports
 from settings import DIRS, RAW_DIR, INTERIM_DIR, PROCESSED_DIR, MODELS_DIR
 
 
-def task_make_dirs():
+def task_makedirs():
     """Make directories if they don't exist"""
     for dir_ in DIRS:
-        yield dict(name=dir_,
-                   actions=[(create_folder, [dir_])],
-                   uptodate=[run_once])
+        yield dict(name=dir_, actions=[(create_folder, [dir_])], uptodate=[run_once])
 
 
 def task_etl_acs():
@@ -26,6 +26,8 @@ def task_etl_acs():
     https://gist.githubusercontent.com/erikbern/89c5f44bd1354854a8954fa2df04453d/raw/efd7b7c31d781a5cae9849be60ab86967bf7d2ed/american_community_survey_example.py
     Author of that underlying code is Erik Bernhardsson | erikbern | https://gist.github.com/erikbern
     """
+    cmd = "python src/etl_acs.py"
+    return dict(actions=[cmd], task_dep=["makedirs"], verbosity=2, clean=True)
     pass
 
 
@@ -36,4 +38,3 @@ def task_etl_tiger():
 def task_etl_he():
     """Pull data from """
     pass
-
---
+++ src/etl_acs.py
@@ -0,0 +1,21 @@
+# standard library imports
+import argparse
+import os
+from pathlib import Path
+# third-party imports
+from loguru import logger
+import numpy as np
+import pandas as pd 
+
+
+if __name__ == '__main__':
+    # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    # initialize logger
+    logger.add(f'log_{Path(__file__).stem}.log')
+
+    # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    # parse arguments
+
+    print('asdf')
+
+
